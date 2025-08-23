463504ca73e002803af840d721d7591aa04f80fe
trishi880988 <gauravyt8803@gmail.com>
2025-08-23 09:23:16 -0700

    Update func.py

--- devgagan/core/func.py
+++ devgagan/core/func.py
@@ -43,10 +43,10 @@ async def subscribe(app, message):
             return 1
       except UserNotParticipant:
         caption = f"Join our channel to use the bot"
-        await message.reply_photo(photo="https://graph.org/file/d44f024a08ded19452152.jpg",caption=caption, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now...", url=f"{url}")]]))
+        await message.reply_photo(photo="https://i.ibb.co/Fq4bKnKP/x.jpg",caption=caption, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now...", url=f"{url}")]]))
         return 1
       except Exception:
-         await message.reply_text("Something Went Wrong. Contact us @devgaganin...")
+         await message.reply_text("Something Went Wrong. Contact us @YTBR_67...")
          return 1
 async def get_seconds(time_string):
     def extract_value_and_unit(ts):
